be824079b5f62da2244b39e373afef1c491d77ca
s <s>
2021-04-28 13:02:53 -0300

    cambios en cnexion con base de datos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,11 +14,11 @@ module.exports={
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "b8d838914ff1f4",
+    "password": "e349075a",
+    "database":"heroku_88112d85e790075" ,
+    "host":"us-cdbr-east-03.cleardb.com" ,
+    "dialect": "mysql",
   }
 }
 
