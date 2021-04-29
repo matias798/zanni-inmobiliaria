fc275d76382043580bae412cab9a1f29b415d2db
s <s>
2021-04-29 17:54:08 -0300

    cree ruta y controlador que retornan un json con las propiedades que estan en alquiler y cuales son las que estan en venta

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -55,8 +55,63 @@ detalle: function (req, res) {
  // Redirecciono a productos
  res.redirect("/incia-sesion");
     })
-}
+},
 /* /indice de productos*/
+
+
+
+/* Propiedades en alquiler */
+alquiler: function (req, res) {
+
+  // Busco todas las propiedades 
+  db.propiedades.findAll(
+    {where:{rolee_id:2}},
+    {
+      // Busco categorias y asociaciones
+      include:[{association:"categorias"},{association:"operaciones"}]
+    }
+  )
+
+.then((propiedad) => {
+  res.json(propiedad)
+})
+
+ .catch((error) => {
+ // muestro el error por consola
+ console.log(error);
+ 
+ // Redirecciono a productos
+ res.redirect("/incia-sesion");
+    })
+},
+/* /Propiedades en alquiler */
+
+/* Propiedades en venta*/
+venta: function (req, res) {
+
+  // Busco todas las propiedades 
+  db.propiedades.findAll(
+    {where:{rolee_id:1}},
+    {
+      // Busco categorias y asociaciones
+      include:[{association:"categorias"},{association:"operaciones"}]
+    }
+  )
+
+.then((propiedad) => {
+  res.json(propiedad)
+})
+
+ .catch((error) => {
+ // muestro el error por consola
+ console.log(error);
+ 
+ // Redirecciono a productos
+ res.redirect("/incia-sesion");
+    })
+},
+/* Propiedades en venta*/
+
 
 
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,5 +11,13 @@ router.get('/', indexController.Indice);
 router.get('/detalle/:id', indexController.detalle);
 /* detalle de productos*/
 
+/* Propiedades en alquiler */
+router.get('/propiedades/alquiler', indexController.alquiler);
+/* Propiedades en alquiler*/
+
+/* Propiedades en venta*/
+router.get('/propiedades/venta', indexController.venta);
+/* Propiedades en venta */
+
 
 module.exports = router;
