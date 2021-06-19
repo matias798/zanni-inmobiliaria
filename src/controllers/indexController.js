672c6180935826ed0be8d5f410962edfde997ee3
matias <matias798>
2021-06-19 19:00:29 -0300

    used prettifier

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,134 +1,128 @@
 const db = require("../database/models");
 const { Op, where } = require("sequelize");
 
-
 let indexController = {
+  /* indice de productos*/
+  Indice: function (req, res) {
+    // Busco todas las propiedades
+    db.propiedades
+      .findAll({
+        // Busco categorias y asociaciones
+        include: [
+          { association: "categorias" },
+          { association: "operaciones" },
+        ],
+      })
 
-/* indice de productos*/
-Indice: function (req, res) {
+      .then((propiedad) => {
+        res.json(propiedad);
+      })
 
-  // Busco todas las propiedades 
-  db.propiedades.findAll(
-    {
-      // Busco categorias y asociaciones
-      include:[{association:"categorias"},{association:"operaciones"}]
-    }
-  )
+      .catch((error) => {
+        // muestro el error por consola
+        console.log(error);
 
-.then((propiedad) => {
-// res.send(propiedad)
-res.json(propiedad)
-})
+        // Redirecciono a productos
+        res.redirect("/incia-sesion");
+      });
+  },
+  /* /indice de productos*/
 
- .catch((error) => {
- // muestro el error por consola
- console.log(error);
- 
- // Redirecciono a productos
- res.redirect("/incia-sesion");
-    })
-},
-/* /indice de productos*/
+  detalle: function (req, res) {
+    // Busco todas las propiedades
+    db.propiedades
+      .findOne(
+        { where: { idpropiedad: req.params.id } },
+        {
+          // Busco categorias y asociaciones
+          include: [
+            { association: "categorias" },
+            { association: "operaciones" },
+          ],
+        }
+      )
 
+      .then((propiedad) => {
+        db.images
+          .findAll({ where: { propiedades_id: propiedad.idpropiedad } })
 
+          .then((resp) => {
+            res.send({ propiedad, resp });
+          })
 
+          .catch((e) => {
+            console.log(e);
+          });
+        // res.json(propiedad)
+      })
 
-detalle: function (req, res) {
+      .catch((error) => {
+        // muestro el error por consola
+        console.log(error);
 
-  // Busco todas las propiedades 
-  db.propiedades.findOne(
-    {where:{idpropiedad:req.params.id}},
-    {
-      // Busco categorias y asociaciones
-      include:[{association:"categorias"},{association:"operaciones"}]
-    }
-  )
+        // Redirecciono a productos
+        res.redirect("/incia-sesion");
+      });
+  },
+  /* /indice de productos*/
 
-.then((propiedad) => {
+  /* Propiedades en alquiler */
+  alquiler: function (req, res) {
+    // Busco todas las propiedades
+    db.propiedades
+      .findAll(
+        { where: { rolee_id: 2 } },
+        {
+          // Busco categorias y asociaciones
+          include: [
+            { association: "categorias" },
+            { association: "operaciones" },
+          ],
+        }
+      )
 
-db.images.findAll({where:{propiedades_id:propiedad.idpropiedad}})
+      .then((propiedad) => {
+        res.json(propiedad);
+      })
 
-.then( resp=>{res.send({propiedad,resp})})
+      .catch((error) => {
+        // muestro el error por consola
+        console.log(error);
 
-.catch(e=>{console.log(e);})
-  // res.json(propiedad)
+        // Redirecciono a productos
+        res.redirect("/incia-sesion");
+      });
+  },
+  /* /Propiedades en alquiler */
 
+  /* Propiedades en venta*/
+  venta: function (req, res) {
+    // Busco todas las propiedades
+    db.propiedades
+      .findAll(
+        { where: { rolee_id: 1 } },
+        {
+          // Busco categorias y asociaciones
+          include: [
+            { association: "categorias" },
+            { association: "operaciones" },
+          ],
+        }
+      )
 
+      .then((propiedad) => {
+        res.json(propiedad);
+      })
 
-})
+      .catch((error) => {
+        // muestro el error por consola
+        console.log(error);
 
- .catch((error) => {
- // muestro el error por consola
- console.log(error);
- 
- // Redirecciono a productos
- res.redirect("/incia-sesion");
-    })
-},
-/* /indice de productos*/
-
-
-
-/* Propiedades en alquiler */
-alquiler: function (req, res) {
-
-  // Busco todas las propiedades 
-  db.propiedades.findAll(
-    {where:{rolee_id:2}},
-    {
-      // Busco categorias y asociaciones
-      include:[{association:"categorias"},{association:"operaciones"}]
-    }
-  )
-
-.then((propiedad) => {
-  res.json(propiedad)
-})
-
- .catch((error) => {
- // muestro el error por consola
- console.log(error);
- 
- // Redirecciono a productos
- res.redirect("/incia-sesion");
-    })
-},
-/* /Propiedades en alquiler */
-
-/* Propiedades en venta*/
-venta: function (req, res) {
-
-  // Busco todas las propiedades 
-  db.propiedades.findAll(
-    {where:{rolee_id:1}},
-    {
-      // Busco categorias y asociaciones
-      include:[{association:"categorias"},{association:"operaciones"}]
-    }
-  )
-
-.then((propiedad) => {
-  res.json(propiedad)
-})
-
- .catch((error) => {
- // muestro el error por consola
- console.log(error);
- 
- // Redirecciono a productos
- res.redirect("/incia-sesion");
-    })
-},
-/* Propiedades en venta*/
-
-
-
-}
-
-
-
-
-
-
+        // Redirecciono a productos
+        res.redirect("/incia-sesion");
+      });
+  },
+  /* Propiedades en venta*/
+};
 
 module.exports = indexController;
